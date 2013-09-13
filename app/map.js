9114c0a1c395dfb9170345d2dca92d82e0c47f05
facu ferrari <facuferrari@gmail.com>
2013-09-12 21:14:00 -0300

    Fixed maps markers and added some styling

--- app/map.js
+++ app/map.js
@@ -6,25 +6,42 @@ function onGoogleReady() {
 angular.module('Maptesting', ['ui.map', 'ui.event'])
 	.controller('CtrlGMap', ['$scope', function($scope) {
 		
+		$scope.myMarker = null;
+
 		$scope.mapOptions = {
 			center: new google.maps.LatLng(-54.798112, -68.303375),
 			zoom: 11,
-			mapTypeId: google.maps.MapTypeId.SATELLITE
+			mapTypeId: google.maps.MapTypeId.SATELLITE,
+			disableDefaultUI: true
 		};
 
-		$scope.myMarkers = [];
+		$scope.addMarker = function ($event, $params) {
+			if ($scope.myMarker == null) {
+				$scope.myMarker = new google.maps.Marker({
+					map: $scope.myMap,
+			        position: $params[0].latLng,
+			        title: $scope.name
+			        });
+			} else {
+				$scope.myMarker.setMap( null );
+			    $scope.myMarker = new google.maps.Marker({
+					map: $scope.myMap,
+			        position: $params[0].latLng,
+			        title: $scope.name
+			        });
+			}
 
-
-		$scope.addMarker = function ($event) {
-	    	$scope.myMarkers.push(new google.maps.Markers({
-	    		map: $scope.myMap,
-	    		position: $event.latLng
-	    	}));
+			//Set the inputs to the map lat and lng
+	    	$scope.lat = $params[0].latLng.lat();
+	    	$scope.lng = $params[0].latLng.lng();
 	    };
 
-	    $scope.markerClicked = function(m) {
-	        window.alert("clicked");
-    	};
+	    $scope.openMarkerInfo = function(marker) {
+		  $scope.currentMarker = marker;
+		  $scope.currentMarkerLat = marker.getPosition().lat();
+		  $scope.currentMarkerLng = marker.getPosition().lng();
+		  $scope.myInfoWindow.open($scope.myMap, marker);
+		};
 
 
 	}]);
